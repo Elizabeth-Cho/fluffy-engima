6be884b9fd65145eaa13a7a57ad57721d15a1c5a
Elizabeth Cho <zergling13rush@gmail.com>
2016-05-16 09:46:39 -0400

    It's not doing the do

---
+++ src/Runnnnn.java
@@ -0,0 +1,18 @@
+import defenses.*;
+import interfaces.Defense;
+
+public class Runnnnn {
+	public static void main (String[] args)
+	{
+		Defense moat = new Moat();
+		Defense ramparts = new Ramparts();
+		Defense rw = new RockWall();
+		Defense rt = new RoughTerrain();
+		
+		System.out.println("Moat: " + moat.getStrength());
+		System.out.println("Ramparts: " + ramparts.getStrength());
+		System.out.println("Rock Wall: " + rw.getStrength());
+		System.out.println("Rough Terrain: " + rt.getStrength());
+	}
+
+}
