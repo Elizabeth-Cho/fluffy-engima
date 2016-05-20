b0d3aa33aaed80e2bb4b3c8f4aea408605a6152b
Elizabeth Cho <zergling13rush@gmail.com>
2016-05-20 09:47:43 -0400

    Added things to boulders

--- src/Boulder.java
+++ src/Boulder.java
@@ -1,8 +1,15 @@
 public class Boulder {
 	
+	private boolean isThrown;
 	
 	public Boulder()
 	{
-		
+		isThrown = false;
+	}
+	
+	public boolean throwIt()
+	{
+		isThrown = !isThrown;
+		return isThrown;
 	}
 }
